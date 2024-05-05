2fe292f90c8b65c71772b2be553dc304310df0e9
lorloop <gallet.julien@orange.fr>
2024-05-05 23:51:58 +0200

    feat : user login

--- FrontEnd/scripts/app.js
+++ FrontEnd/scripts/app.js
@@ -1,5 +1,10 @@
 const apiUrl = 'http://localhost:5678/api/'
 const portfolio = document.getElementById('portfolio')
+const authToken = window.localStorage.getItem('authToken')
+
+if (authToken) {
+    console.log('User connected')
+}
 
 
 async function getWorks() {
@@ -89,5 +94,51 @@ loginLink.addEventListener('click', () => {
     const login = document.getElementById('login')
     login.style.display = 'block'
 })
+
+const loginForm = document.getElementById('loginForm')
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const email = document.getElementById('loginEmail').value
+    const password = document.getElementById('password').value
+
+    performLogin(email, password)
+})
+
+async function performLogin(userEmail, userPassword) {
+    const message = document.getElementById('errorMessage')
+    if (message) {
+        loginForm.removeChild(message)
+    }
+    const data = {
+        email: userEmail, 
+        password: userPassword,
+    }
+    try {
+        const response = await fetch(apiUrl + 'users/login', {
+            method: "POST", 
+            headers: {
+                "Content-Type": "application/json",
+            }, 
+            body: JSON.stringify(data)
+        })
+        if (response.status == '404' || response.status == '401') {
+            const errorMessage = document.createElement('p')
+            errorMessage.setAttribute('id', 'errorMessage')
+            errorMessage.innerText = 'Erreur dans l’identifiant ou le mot de passe'
+            loginForm.appendChild(errorMessage)
+        } else if (response.status == '200' ) {
+            console.log(response)
+            const data = await response.json()
+            window.localStorage.setItem('authToken', data.token)
+            window.location.href = '/'
+            window.location.reload(true)
+        } else {
+            console.log(response)
+        }
+    } catch(error) {
+        console.log(error)
+    }
+}
 
 getWorks()
